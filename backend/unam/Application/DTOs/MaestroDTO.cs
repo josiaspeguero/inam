82efe011279692ea556a1dc6d60ad91867b4526f
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-05 11:16:55 -0400

    feat: entidad maestro y dto agregados

---
+++ backend/unam/Application/DTOs/MaestroDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace unam.Application.DTOs
+{
+    public class MaestroDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public int Edad { get; set; }
+        public string NoDocumento { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public DateTime FechaIngreso { get; set; }
+    }
+}
