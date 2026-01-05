262b2a25a587f747f68c5898d579af71ac1401bb
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-05 10:58:49 -0400

    chore: estructura de dominio agregada

---
+++ backend/unam/Domain/Entities/Solicitud.cs
@@ -0,0 +1,22 @@
+﻿namespace unam.Domain.Entities
+{
+    public class Solicitud
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+
+        public int Edad { get; set; }
+
+        public string NoDocumento { get; set; } = string.Empty;
+
+        public string Padres { get; set; } = string.Empty;
+
+        public string Domicilio { get; set; } = string.Empty;
+
+        public string NumeroTelefono { get; set; } = string.Empty;
+
+        public DateTime FechaSolicitud { get; set; }
+    }
+}
