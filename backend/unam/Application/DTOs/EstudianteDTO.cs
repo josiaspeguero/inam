2b7192d2720b79de342aac18397e9b676d71a7c3
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-05 11:18:02 -0400

    feat: entidad estudiante y dto agregados

---
+++ backend/unam/Application/DTOs/EstudianteDTO.cs
@@ -0,0 +1,20 @@
+﻿using unam.Domain.Entities;
+
+namespace unam.Application.DTOs
+{
+    public class EstudianteDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Matricula { get; set; } = string.Empty;
+
+        public string CorreoEstudiantil { get; set; } = string.Empty;
+
+        public string Pin { get; set; } = string.Empty;
+
+        public ICollection<Solicitud> Solicitudes { get; set; } = new List<Solicitud>();
+
+        public DateTime FechaIngreso { get; set; }
+
+    }
+}
---
+++ backend/unam/Domain/Entities/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿namespace unam.Domain.Entities
+{
+    public class Estudiante
+    {
+        public Guid Id { get; set; }
+
+        public string Matricula { get; set; } = string.Empty;
+
+        public string CorreoEstudiantil { get; set; } = string.Empty;
+
+        public string Pin { get; set; } = string.Empty;
+
+        public ICollection<Solicitud> Solicitudes { get; set; } = new List<Solicitud>();
+
+        public DateTime FechaIngreso { get; set; }
+
+    }
+}
