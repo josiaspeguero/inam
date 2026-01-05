cd71bea16f13fb421383408455c8ae43e01e4f10
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-05 11:18:48 -0400

    feat: entidad solicitud y dto agregados

---
+++ backend/unam/Application/DTOs/SolicitudDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace unam.Application.DTOs
+{
+    public class SolicitudDTO
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+
+        public int Edad { get; set; }
+
+        public string NoDocumento { get; set; } = string.Empty;
+
+        public string Padres { get; set; } = string.Empty;
+
+        public string Domicilio { get; set; } = string.Empty;
+
+        public string NumeroTelefono { get; set; } = string.Empty;
+
+        public DateTime FechaSolicitud { get; set; }
+    }
+}
