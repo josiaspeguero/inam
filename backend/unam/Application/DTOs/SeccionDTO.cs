c8df957c8847c916ae621319ff3631dbcfcade22
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-05 11:24:31 -0400

    feat: entidad seccion y dto agregados

---
+++ backend/unam/Application/DTOs/SeccionDTO.cs
@@ -0,0 +1,13 @@
+﻿using unam.Domain.Entities;
+
+namespace unam.Application.DTOs
+{
+    public class SeccionDTO
+    {
+        public Guid Id { get; set; }
+
+        public string NombreSeccion { get; set; } = string.Empty;
+
+        public int CupoDisponible { get; set; }
+    }
+}
