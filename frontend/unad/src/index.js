cc8f12d1fd560037a6f1dc987d9ded0dca90034e
josiaspegueroltjnc <josiaspegueroltjnc@gmail.com>
2026-01-13 14:43:41 -0400

    chore: subida inicial del frontend app

---
+++ frontend/unad/src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
